fc7f395ecd1c3b95a9cb2474f0289520333bc2d8
Ayoub KEBAILI <keayoub@msn.com>
2025-07-07 15:32:36 -0400

    update version to 1.0.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ if requirements_file.exists():
 
 setup(
     name='pvw-cli',
-    version="1.0.0",
+    version="1.0.1",
     description="Microsoft Purview CLI with comprehensive automation capabilities",
     long_description=long_description,
     long_description_content_type='text/markdown',
